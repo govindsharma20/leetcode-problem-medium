e6e00eef1c12c6a640e8a29de37e2fc12a163672
Govind Sharma <sharma78392@gmail.com>
2025-07-19 23:47:48 +0530

    Create leetcode-problem-18.java

---
+++ leetcode-problem-18.java
@@ -0,0 +1,63 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        int n = nums.length;
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            if ((long) nums[i] + nums[i+1] + nums[i+2] + nums[i+3] > target) {
+                break;
+            }
+            if ((long) nums[i] + nums[n-1] + nums[n-2] + nums[n-3] < target) {
+                continue;
+            }
+
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                
+                if ((long) nums[i] + nums[j] + nums[j+1] + nums[j+2] > target) {
+                    break;
+                }
+                if ((long) nums[i] + nums[j] + nums[n-1] + nums[n-2] < target) {
+                    continue;
+                }
+
+                int left = j + 1;
+                int right = n - 1;
+                
+                long remainingTarget = (long)target - nums[i] - nums[j];
+
+                while (left < right) {
+                    long currentSum = (long)nums[left] + nums[right];
+
+                    if (currentSum == remainingTarget) {
+                        result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+
+                        while (left < right && nums[left] == nums[left + 1]) {
+                            left++;
+                        }
+                        while (left < right && nums[right] == nums[right - 1]) {
+                            right--;
+                        }
+
+                        left++;
+                        right--;
+                    } else if (currentSum < remainingTarget) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+}
