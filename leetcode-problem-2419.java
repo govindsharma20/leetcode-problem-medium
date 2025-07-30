68f1c9633023bb1526338ae5334b1ac03dea73f8
Govind Sharma <sharma78392@gmail.com>
2025-07-30 22:02:29 +0530

    Create leetcode-problem-2419.java

---
+++ leetcode-problem-2419.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int longestSubarray(int[] nums) {
+        int maxVal = 0;
+        for (int num : nums) {
+            if (num > maxVal) {
+                maxVal = num;
+            }
+        }
+
+        int maxLength = 0;
+        int currentLength = 0;
+
+        for (int num : nums) {
+            if (num == maxVal) {
+                currentLength++;
+            } else {
+                maxLength = Math.max(maxLength, currentLength);
+                currentLength = 0; 
+            }
+        }
+        maxLength = Math.max(maxLength, currentLength);
+        return maxLength;
+    }
+}
